df7ab1c3273c6a44825a9e2f351f3e0817789dce
Matus Horvath <matus.horvath@gmail.com>
2019-12-31 17:09:52 +0100

    .FRAME works in ic

--- 2019/xzintbit/src/ic.s
+++ 2019/xzintbit/src/ic.s
@@ -18,24 +18,24 @@ read:
     arb -3
 
 read_next_digit:
-    in  [rb + 0]
-    eq  [rb + 0], 44, [rb + 2]
-    jnz [rb + 2], read_finish_byte
-    eq  [rb + 0], 10, [rb + 2]
-    jnz [rb + 2], read_finish_byte
-    add [rb + 0], -48, [rb + 0]
-    mul [rb + 1], 10, [rb + 1]
-    add [rb + 1], [rb + 0], [rb + 1]
+    in  [rb + digit]
+    eq  [rb + digit], 44, [rb + flag]
+    jnz [rb + flag], read_finish_byte
+    eq  [rb + digit], 10, [rb + flag]
+    jnz [rb + flag], read_finish_byte
+    add [rb + digit], -48, [rb + digit]
+    mul [rb + byte], 10, [rb + byte]
+    add [rb + byte], [rb + digit], [rb + byte]
     jz  0, read_next_digit
 
 read_finish_byte:
 +3 = read_size:
-    add [rb + 1], 0, [mem]
-    add 0, 0, [rb + 1]
+    add [rb + byte], 0, [mem]
+    add 0, 0, [rb + byte]
     add [read_size], 1, [read_size]
 
-    eq  [rb + 0], 10, [rb + 2]
-    jz  [rb + 2], read_next_digit
+    eq  [rb + digit], 10, [rb + flag]
+    jz  [rb + flag], read_next_digit
 
     add [read_size], -1, [size]
 
@@ -60,16 +60,16 @@ print:
 
 print_byte:
 #+1 = print_size:
-#    add [mem], '0', [rb + 0]
-#    out [rb + 0]
+#    add [mem], '0', [rb + byte]
+#    out [rb + byte]
 
 +1 = print_size:
     add [mem], 0, [rb - 1]
     arb -1
     cal print_num
 
-    eq  [size], [print_size], [rb + 1]
-    jnz [rb + 1], print_finish
+    eq  [size], [print_size], [rb + flag]
+    jnz [rb + flag], print_finish
     add [print_size], 1, [print_size]
 
     out 44
@@ -86,37 +86,37 @@ print_finish:
 ##########
 # convert number to string
 print_num:
-.FRAME tmp, order, digit; num
+.FRAME num; tmp, order, digit
     # also digit[] is on stack (overlaps next stack frame)
     arb -3
 
     # determine highest power of 10
-    add 1, 0, [rb + 1]
+    add 1, 0, [rb + order]
 
 print_num_next_order:
-    mul [rb + 1], 10, [rb + 0]
-    lt  [rb + 0], [rb + 4], [rb + 0]
-    jz  [rb + 0], print_num_next_digit
-    mul [rb + 1], 10, [rb + 1]
+    mul [rb + order], 10, [rb + tmp]
+    lt  [rb + tmp], [rb + num], [rb + tmp]
+    jz  [rb + tmp], print_num_next_digit
+    mul [rb + order], 10, [rb + order]
     jz  0, print_num_next_order
 
 print_num_next_digit:
     out 'N'
-    out [rb + 4]
+    out [rb + num]
     out 'O'
-    out [rb + 1]
+    out [rb + order]
     out 10
 
-#    add 0, 0, [rb + 2]
+#    add 0, 0, [rb + digit]
 #
 #print_num_next_order:
-#    add [rb + 2], 1, [rb + 2]
-#    mul [rb + 2], [rb + 1], [rb + 0]
-#    lt  [rb + 0], [rb + 1], [rb + 0]
-#    jnz [rb + 0], print_num_next_order
+#    add [rb + digit], 1, [rb + digit]
+#    mul [rb + digit], [rb + order], [rb + tmp]
+#    lt  [rb + tmp], [rb + order], [rb + tmp]
+#    jnz [rb + tmp], print_num_next_order
 #
 #+3 = print_num_digit_index:
-#    add [rb + 2], -1, [rb - 1]
+#    add [rb + digit], -1, [rb - 1]
 #    add [print_num_digit_index], -1, [print_num_digit_index]
 #    j
 
