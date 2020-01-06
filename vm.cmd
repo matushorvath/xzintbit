be87ed5bd1d29ef0b93c3838bd8f588696589bed
Matus Horvath <matus.horvath@gmail.com>
2020-01-06 22:08:18 +0100

    switch to c vm

--- vm.cmd
+++ vm.cmd
@@ -1,28 +1,16 @@
-@ECHO OFF
+REM @ECHO OFF
 SETLOCAL
 
 REM Edit this script to run your ASCII-enabled IntCode VM
 REM It should read input from stdin and write output to stdout (with no extra characters)
 
-PUSHD vm
-
-REM sanity check, node.js must be installed
-node --version > NUL
-IF ERRORLEVEL 1 (
-    ECHO You will need Node.js version 10.x or newer to run the Intcode VM >&2
-    ECHO Or you can use your own Intcode VM, see https://tinyurl.com/vq59mtk >&2
-    EXIT /B 1
-)
-
-REM install modules if they don't exist
-IF NOT EXIST node_modules (
-    ECHO First run, installing TypeScript modules >&2
-    CMD /C npm i >&2
+REM build the VM
+IF NOT EXIST vm\ic.exe (
+    ECHO First run, building the Intcode VM >&2
+    make -C vm >&2
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
 REM execute the vm
-CMD /C npx ts-node src\ic.ts "..\%1"
+vm\ic.exe "%1"
 IF ERRORLEVEL 1 EXIT /B 1
-
-POPD
