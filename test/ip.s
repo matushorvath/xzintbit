5bebd779129151946b13955dbfaf4d69b1179cc6
Matus Horvath <matus.horvath@gmail.com>
2020-01-06 17:38:18 +0100

    ip symbol

---
+++ test/ip.s
@@ -0,0 +1,39 @@
+ds  7, 0
+
+out ip
+out ip + 0
+out ip + 5
+out ip + +7
+out ip + -3
+out ip + 'x'
+out ip + +'y'
+out ip + -'a'
+
+out [ip]
+out [ip + 0]
+out [ip + 5]
+out [ip + +7]
+out [ip + -3]
+out [ip + 'x']
+out [ip + +'y']
+out [ip + -'a']
+
+out [rb + ip]
+
+out [rb + ip + 0]
+out [rb + ip + 5]
+out [rb + ip + +7]
+out [rb + ip + -3]
+out [rb + ip + 'x']
+out [rb + ip + +'y']
+out [rb + ip + -'a']
+
+out [rb + ip - 0]
+out [rb + ip - 5]
+out [rb + ip - +7]
+out [rb + ip - -3]
+out [rb + ip - 'x']
+out [rb + ip - +'y']
+out [rb + ip - -'a']
+
+.EOF
