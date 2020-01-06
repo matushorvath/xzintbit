9a68ee672d6aacf545097e9e7a12219c1bb828aa
Matus Horvath <matus.horvath@sap.com>
2020-01-06 04:08:17 +0100

    simplify vm

--- vm/src/vm.ts
+++ vm/src/vm.ts
@@ -1,8 +1,3 @@
-interface Op {
-    oc: number;
-    mds: number[];
-};
-
 export class Vm {
     constructor(public id: number, private mem: number[]) {
         this.ip = 0;
@@ -12,16 +7,9 @@ export class Vm {
     public ip: number;
     private rb: number;
 
-    private getOp = () => {
-        const val = this.mem[this.ip] ?? 0;
-        return {
-            oc: val % 100,
-            mds: [100, 1000, 10000].map(i => Math.trunc(val / i) % 10)
-        }
-    };
-
-    private getParam = (o: Op, idx: number) => {
-        switch (o.mds[idx]) {
+    private getParam = (idx: number) => {
+        const mode = Math.trunc((this.mem[this.ip] ?? 0) / [100, 1000, 10000][idx]) % 10;
+        switch (mode) {
             case 0: { // position mode
                 return this.mem[this.mem[this.ip + idx + 1] ?? 0] ?? 0;
             }
@@ -32,12 +20,13 @@ export class Vm {
                 return this.mem[this.rb + this.mem[this.ip + idx + 1] ?? 0] ?? 0;
             }
             default:
-                throw new Error(`mode error: mem ${this.mem} ip ${this.ip} o ${o} idx ${idx}`);
+                throw new Error(`mode error: mem ${this.mem} ip ${this.ip} idx ${idx}`);
         }
     };
 
-    private setParam = (o: Op, idx: number, val: number) => {
-        switch (o.mds[idx]) {
+    private setParam = (idx: number, val: number) => {
+        const mode = Math.trunc((this.mem[this.ip] ?? 0) / [100, 1000, 10000][idx]) % 10;
+        switch (mode) {
             case 0: { // position mode
                 this.mem[this.mem[this.ip + idx + 1] ?? 0] = val;
                 break;
@@ -47,21 +36,21 @@ export class Vm {
                 break;
             }
             default:
-                throw new Error(`mode error: mem ${this.mem} ip ${this.ip} o ${o} idx ${idx}`);
+                throw new Error(`mode error: mem ${this.mem} ip ${this.ip} idx ${idx}`);
         }
     };
 
     run = async function* (ins: AsyncGenerator<number> = (async function* () {})()): AsyncGenerator<number> {
         while (true) {
-            const o = this.getOp();
+            const oc = (this.mem[this.ip] ?? 0) % 100;
 
-            switch (o.oc) {
+            switch (oc) {
                 case 1: // add
-                    this.setParam(o, 2, this.getParam(o, 0) + this.getParam(o, 1));
+                    this.setParam(2, this.getParam(0) + this.getParam(1));
                     this.ip += 4;
                     break;
                 case 2: // mul
-                    this.setParam(o, 2, this.getParam(o, 0) * this.getParam(o, 1));
+                    this.setParam(2, this.getParam(0) * this.getParam(1));
                     this.ip += 4;
                     break;
                 case 3: { // in
@@ -69,46 +58,46 @@ export class Vm {
                     if (done) {
                         throw new Error('no more inputs');
                     }
-                    this.setParam(o, 0, value);
+                    this.setParam(0, value);
                     this.ip += 2;
                     break;
                 }
                 case 4: { // out
-                    const value = this.getParam(o, 0);
+                    const value = this.getParam(0);
                     this.ip += 2;
                     yield value;
                     break;
                 }
                 case 5: // jnz
-                    if (this.getParam(o, 0) !== 0) {
-                        this.ip = this.getParam(o, 1)
+                    if (this.getParam(0) !== 0) {
+                        this.ip = this.getParam(1)
                     } else {
                         this.ip += 3;
                     }
                     break;
                 case 6: // jz
-                    if (this.getParam(o, 0) === 0) {
-                        this.ip = this.getParam(o, 1)
+                    if (this.getParam(0) === 0) {
+                        this.ip = this.getParam(1)
                     } else {
                         this.ip += 3;
                     }
                     break;
                 case 7: // lt
-                    this.setParam(o, 2, this.getParam(o, 0) < this.getParam(o, 1) ? 1 : 0);
+                    this.setParam(2, this.getParam(0) < this.getParam(1) ? 1 : 0);
                     this.ip += 4;
                     break;
                 case 8: // eq
-                    this.setParam(o, 2, this.getParam(o, 0) === this.getParam(o, 1) ? 1 : 0);
+                    this.setParam(2, this.getParam(0) === this.getParam(1) ? 1 : 0);
                     this.ip += 4;
                     break;
                 case 9: // arb
-                    this.rb += this.getParam(o, 0);
+                    this.rb += this.getParam(0);
                     this.ip += 2;
                     break;
                 case 99: // hlt
                     return;
                 default:
-                    throw new Error(`opcode error: mem ${this.mem} ip ${this.ip} oc ${o.oc}`);
+                    throw new Error(`opcode error: mem ${this.mem} ip ${this.ip} oc ${oc}`);
             }
         }
     };
