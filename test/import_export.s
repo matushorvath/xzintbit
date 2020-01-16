a0ab9a979d0374fc9c811233b0f0f7f89ebeb5bf
Matus Horvath <matus.horvath@gmail.com>
2020-01-16 22:40:23 +0100

    compile to object files

--- test/import_export.s
+++ test/import_export.s
@@ -9,12 +9,15 @@ symbol0:
 .EXPORT symbol1
 
 .SYMBOL constant 123
-.EXPORT constant
 
 .IMPORT symbol2
 db  symbol2 + 1
 db  symbol3 + 2
 .IMPORT symbol3
 out [symbol2 - 7]
+
+local0:
+    out local0 + 1
+    out [local0 + 2]
 
 .EOF
