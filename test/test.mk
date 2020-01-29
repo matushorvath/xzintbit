821fdbf82b36a42ae8b9aafd80ebe1785073c256
Matus Horvath <matus.horvath@gmail.com>
2020-01-29 04:10:30 +0100

    windows makefiles

--- test/test.mk
+++ test/test.mk
@@ -1,19 +1,11 @@
-ifeq ($(OS), Windows_NT)
-	RMRF = RMDIR /S /Q $1
-	MKDIR = $(if $(wildcard $1),,MKDIR $1)
-else
-	MKDIR = mkdir -p $1
-	RMRF = rm -rf $1
-endif
-
 NAME = $(notdir $(CURDIR))
 
 ICAS = $(realpath ../../vm/ic) $(realpath ../../bin/as.input)
 ICLD = $(realpath ../../vm/ic) $(realpath ../../bin/ld.input)
 
 prep:
-	$(RMRF) output
-	$(MKDIR) output
+	rm -rf output
+	mkdir -p output
 
 output/%.input: output/%.o
 	echo .$$ | cat $^ - | $(ICLD) > $@ 2> /dev/null || true
@@ -28,6 +20,6 @@ output/%.o: %.s
 	diff $(notdir $@) $@ > /dev/null 2> /dev/null || diff $(notdir $@) $@
 
 clean:
-	$(RMRF) output
+	rm -rf output
 
 .PHONY: prep clean
