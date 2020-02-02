6776ace65c1f64b703e625f5c6f3940f4c8174fb
Matus Horvath <matus.horvath@gmail.com>
2020-02-02 19:12:26 +0100

    Colored logging

--- test/test.mk
+++ test/test.mk
@@ -1,25 +1,43 @@
 NAME = $(notdir $(CURDIR))
+OUTDIR = output
+LOGFILE = ../test.log
 
 ICAS = $(abspath ../../vm)/ic $(abspath ../../bin/as.input)
 ICLD = $(abspath ../../vm)/ic $(abspath ../../bin/ld.input)
 
+HAVE_COLOR := $(or $(FORCE_COLOR), $(shell [ -n $$(tput colors) ] && [ $$(tput colors) -ge 8 ] && echo 1))
+ifeq ($(HAVE_COLOR),1)
+	COLOR_NORMAL := "$(shell tput sgr0)"
+	COLOR_RED := "$(shell tput setaf 1)"
+	COLOR_GREEN := "$(shell tput setaf 2)"
+endif
+
 prep:
-	rm -rf output
-	mkdir -p output
+	rm -rf $(OUTDIR)
+	mkdir -p $(OUTDIR)
 
-output/%.input: output/%.o
+$(OUTDIR)/%.input: $(OUTDIR)/%.o
+	echo -n 'Linking $(NAME): ' >> $(LOGFILE)
 	echo .$$ | cat $^ - | $(ICLD) > $@ 2> /dev/null || true
-	diff $(notdir $@) $@ > /dev/null 2> /dev/null || diff $(notdir $@) $@
+	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
+		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
+	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
-output/%.a: output/%.o
+$(OUTDIR)/%.a: $(OUTDIR)/%.o
+	echo -n 'Archiving $(NAME): ' >> $(LOGFILE)
 	echo .L | cat - $^ > $@ || true
-	diff $(notdir $@) $@ > /dev/null 2> /dev/null || diff $(notdir $@) $@
+	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
+		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
+	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
-output/%.o: %.s
+$(OUTDIR)/%.o: %.s
+	echo -n 'Assembling $(NAME): ' >> $(LOGFILE)
 	$(ICAS) < $< > $@ 2> /dev/null || true
-	diff $(notdir $@) $@ > /dev/null 2> /dev/null || diff $(notdir $@) $@
+	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
+		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
+	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
 clean:
-	rm -rf output
+	rm -rf $(OUTDIR)
 
 .PHONY: prep clean
