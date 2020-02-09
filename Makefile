559cb1aebc2ac1b92872132fbaccc52ce0042194
Matus Horvath <matus.horvath@gmail.com>
2020-02-09 19:32:20 +0100

    refactor makefiles

--- Makefile
+++ Makefile
@@ -1,55 +1,46 @@
 TESTDIRS = $(sort $(dir $(wildcard test/*/*)))
 CFLAGS = -O3 -Wall -Werror -std=c11
 
-build: build-vm build-stage1 build-stage2 compare-stages
+.PHONY: build
+build: build-vm build-stage1 build-stage2 compare-stages install
 
 # Build Intcode VM
+.PHONY: build-vm
 build-vm: vm/ic
 vm/ic: vm/ic.o
 
 # Build stage 1
-build-stage1: stage1 stage1/as.input stage1/ld.input
+.PHONY: build-stage1
+build-stage1:
+	ICAS=$(abspath bin/as.input) \
+	ICLD=$(abspath bin/ld.input) \
+	BINDIR=$(abspath stage1) \
+	OBJDIR=$(abspath stage1) \
+	make -C src build
 
-stage1:
-	mkdir -p stage1
-
-stage1/as.input: stage1/as.o
-
-stage1/ld.input: stage1/ld.o
-
-stage1/%.input: stage1/%.o
-	echo .$$ | cat $^ - | vm/ic bin/ld.input > $@ || ( cat $@ ; false )
-
-stage1/%.o: src/%.s
-	vm/ic bin/as.input < $< > $@ || ( cat $@ ; false )
-
-# Build stage 1
-build-stage2: build-stage1 stage2 stage2/as.input stage2/ld.input
-
-stage2:
-	mkdir -p stage2
-
-stage2/as.input: stage2/as.o
-
-stage2/ld.input: stage2/ld.o
-
-stage2/%.input: stage2/%.o
-	echo .$$ | cat $^ - | vm/ic stage1/ld.input > $@ || ( cat $@ ; false )
-
-stage2/%.o: src/%.s
-	vm/ic stage1/as.input < $< > $@ || ( cat $@ ; false )
+# Build stage 2
+.PHONY: build-stage2
+build-stage2:
+	ICAS=$(abspath stage1/as.input) \
+	ICLD=$(abspath stage1/ld.input) \
+	BINDIR=$(abspath stage2) \
+	OBJDIR=$(abspath stage2) \
+	make -C src build
 
 # Compare and install
+.PHONY: compare-stages
 compare-stages:
 	diff -r stage1/as.input stage2/as.input
 	diff -r stage1/ld.input stage2/ld.input
 
-install: build
+.PHONY: install
+install:
 	cp stage2/as.input bin/as.input
 	cp stage2/ld.input bin/ld.input
 
 # Test
-test: install
+.PHONY: install
+test: build
 	rm -rf test/test.log
 	failed=0 ; \
 	for testdir in $(TESTDIRS) ; do \
@@ -59,11 +50,10 @@ test: install
 	[ $$failed = 0 ] || exit 1
 
 # Clean
+.PHONY: clean
 clean:
 	for testdir in $(TESTDIRS) ; do $(MAKE) -C $$testdir clean ; done
 	rm -rf test/test.log
 	rm -rf *.tmp
 	rm -rf stage1 stage2
 	rm -rf vm/ic vm/ic.exe vm/ic.o
-
-.PHONY: build build-vm build-stage1 build-stage2 compare-stages test install clean
--- test/test.mk
+++ test/test.mk
@@ -1,9 +1,12 @@
-NAME = $(notdir $(CURDIR))
-OUTDIR = output
-LOGFILE = ../test.log
+ICVM ?= $(abspath ../../vm)/ic
+ICAS ?= $(abspath ../../bin/as.input)
+ICLD ?= $(abspath ../../bin/ld.input)
 
-ICAS = $(abspath ../../vm)/ic $(abspath ../../bin/as.input)
-ICLD = $(abspath ../../vm)/ic $(abspath ../../bin/ld.input)
+BINDIR ?= bin
+OBJDIR ?= obj
+
+NAME = $(notdir $(CURDIR))
+LOGFILE = ../test.log
 
 HAVE_COLOR := $(or $(FORCE_COLOR), $(shell [ -n $$(tput colors) ] && [ $$(tput colors) -ge 8 ] && echo 1))
 ifeq ($(HAVE_COLOR),1)
@@ -12,40 +15,43 @@ ifeq ($(HAVE_COLOR),1)
 	COLOR_GREEN := "$(shell tput setaf 2)"
 endif
 
+.PHONY: default test
 default: test
 
-prep:
-	rm -rf $(OUTDIR)
-	mkdir -p $(OUTDIR)
+.PHONY: test-prep
+test-prep:
+	rm -rf $(BINDIR) $(OBJDIR)
+	mkdir -p $(BINDIR) $(OBJDIR)
 
-$(OUTDIR)/%.input: $(OUTDIR)/%.o
+$(BINDIR)/%.input: $(OBJDIR)/%.o
 	echo -n '$(NAME): linking ' >> $(LOGFILE)
-	echo .$$ | cat $^ - | $(ICLD) > $@ 2> /dev/null || true
+	echo .$$ | cat $^ - | $(ICVM) $(ICLD) > $@ 2> /dev/null || true
 	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
 		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
 	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
-$(OUTDIR)/%.a: $(OUTDIR)/%.o
+$(BINDIR)/%.a: $(OBJDIR)/%.o
 	echo -n '$(NAME): archiving ' >> $(LOGFILE)
 	echo .L | cat - $^ > $@ || true
 	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
 		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
 	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
-$(OUTDIR)/%.o: %.s
+$(OBJDIR)/%.o: %.s
 	echo -n '$(NAME): assembling ' >> $(LOGFILE)
-	$(ICAS) < $< > $@ 2> /dev/null || true
+	$(ICVM) $(ICAS) < $< > $@ 2> /dev/null || true
 	@diff $(notdir $@) $@ > /dev/null 2> /dev/null || \
 		( echo $(COLOR_RED)FAILED$(COLOR_NORMAL) ; diff $(notdir $@) $@ ) >> $(LOGFILE)
 	@echo $(COLOR_GREEN)OK$(COLOR_NORMAL) >> $(LOGFILE)
 
+.PHONY: skip
 skip:
 	@echo $(NAME): $(COLOR_RED)SKIPPED$(COLOR_NORMAL) >> $(LOGFILE)
 	false
 
+.PHONY: clean
 clean:
-	rm -rf $(OUTDIR)
+	rm -rf $(BINDIR) $(OBJDIR)
 
-.PHONY: default prep skip clean
-
+# Keep all automatically generated files (e.g. object files)
 .SECONDARY:
