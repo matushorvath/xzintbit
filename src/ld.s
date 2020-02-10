0c9d4b5d941b430d3cde1039e07a03600af0a0be
Matus Horvath <matus.horvath@sap.com>
2020-02-10 15:56:38 +0100

    ld print.s

--- src/ld.s
+++ src/ld.s
@@ -1,4 +1,9 @@
+# from builtin
 .IMPORT __heap_start
+
+# from print.s
+.IMPORT print_num
+.IMPORT print_str
 
 ##########
     arb stack
@@ -1505,86 +1510,6 @@ report_error_with_symbol_msg_start:
     db "Error: ", 0
 report_error_with_symbol_msg_symbol:
     db ": ", 0
-.ENDFRAME
-
-##########
-print_str:
-.FRAME str; index, tmp, char
-    arb -3
-
-    add 0, 0, [rb + index]
-
-print_str_loop:
-    add [rb + str], [rb + index], [ip + 1]
-    add [0], 0, [rb + char]
-
-    jz  [rb + char], print_str_done
-
-    out [rb + char]
-
-    add [rb + index], 1, [rb + index]
-    jz  0, print_str_loop
-
-print_str_done:
-    arb 3
-    ret 1
-.ENDFRAME
-
-##########
-# convert number to string
-print_num:
-.FRAME num; tmp, order, digit; digits
-    arb -3
-
-    # determine highest power of 10
-    add 1, 0, [rb + order]
-
-    # handle sign if negative
-    lt  [rb + num], 0, [rb + tmp]
-    jz  [rb + tmp], print_num_next_order
-    out '-'
-    mul [rb + num], -1, [rb + num]
-
-print_num_next_order:
-+3 = print_num_digit_ptr_1:
-    add [rb + order], 0, [rb + digits]
-    add [print_num_digit_ptr_1], -1, [print_num_digit_ptr_1]
-
-    mul [rb + order], 10, [rb + order]
-    lt  [rb + num], [rb + order], [rb + tmp]
-    jz  [rb + tmp], print_num_next_order
-
-print_num_finish_order:
-    add [print_num_digit_ptr_1], 1, [print_num_digit_ptr_2]
-
-print_num_next_digit:
-+1 = print_num_digit_ptr_2:
-    add [rb + digits], 0, [rb + order]
-    add -1, 0, [rb + digit]
-
-print_num_increase:
-    add [rb + digit], 1, [rb + digit]
-    mul [rb + order], -1, [rb + tmp]
-    add [rb + num], [rb + tmp], [rb + num]
-    lt  [rb + num], 0, [rb + tmp]
-    jz  [rb + tmp], print_num_increase
-
-    add [rb + num], [rb + order], [rb + num]
-    add [rb + digit], '0', [rb + digit]
-    out [rb + digit]
-
-    eq  [rb + order], 1, [rb + tmp]
-    jnz [rb + tmp], print_num_finish
-
-    add [print_num_digit_ptr_2], 1, [print_num_digit_ptr_2]
-    jz  0, print_num_next_digit
-
-print_num_finish:
-    add digits, 0, [print_num_digit_ptr_2]
-    add digits, 0, [print_num_digit_ptr_1]
-
-    arb 3
-    ret 1
 .ENDFRAME
 
 ##########
