61289298eac1474f4143b98d3e57a17c4627fdb0
Matus Horvath <matus.horvath@sap.com>
2020-02-10 16:43:13 +0100

    as string.s

--- src/as.s
+++ src/as.s
@@ -25,6 +25,13 @@
 .IMPORT alloc
 .IMPORT free
 
+# from string.s
+.IMPORT is_digit
+.IMPORT is_alpha
+.IMPORT is_alphanum
+.IMPORT strcmp
+.IMPORT strcpy
+
 ##########
     arb stack
 
@@ -1676,72 +1683,6 @@ is_symbol_end:
 .ENDFRAME
 
 ##########
-is_digit:
-.FRAME char; tmp
-    arb -1
-
-    # check if 0 <= char <= 9
-    lt  '9', [rb + char], [rb + tmp]                        # if char > 9, not a digit
-    jnz [rb + tmp], is_digit_end
-    lt  [rb + char], '0', [rb + tmp]                        # if char < 0, not a digit
-
-is_digit_end:
-    # the result is a logical negation of [rb + tmp]
-    eq  [rb + tmp], 0, [rb + tmp]
-
-    arb 1
-    ret 1
-.ENDFRAME
-
-##########
-is_alpha:
-.FRAME char; tmp
-    arb -1
-
-    # check if a <= char <= z
-    lt  'z', [rb + char], [rb + tmp]                        # if char > z, not a letter
-    jnz [rb + tmp], is_alpha_end
-    lt  [rb + char], 'a', [rb + tmp]                        # if !(char < a), is a letter
-    jz  [rb + tmp], is_alpha_end
-
-    # check if A <= char <= Z
-    lt  'Z', [rb + char], [rb + tmp]                        # if char > Z, not a letter
-    jnz [rb + tmp], is_alpha_end
-    lt  [rb + char], 'A', [rb + tmp]                        # if !(char < A), is a letter
-
-is_alpha_end:
-    # the result is a logical negation of [rb + tmp]
-    eq  [rb + tmp], 0, [rb + tmp]
-
-    arb 1
-    ret 1
-.ENDFRAME
-
-##########
-is_alphanum:
-.FRAME char; tmp
-    arb -1
-
-    add [rb + char], 0, [rb - 1]
-    arb -1
-    call is_alpha
-    add [rb - 3], 0, [rb + tmp]
-    jnz [rb + tmp], is_alphanum_end
-
-    add [rb + char], 0, [rb - 1]
-    arb -1
-    call is_digit
-    add [rb - 3], 0, [rb + tmp]
-    jnz [rb + tmp], is_alphanum_end
-
-    eq  [rb + char], '_', [rb + tmp]
-
-is_alphanum_end:
-    arb 1
-    ret 1
-.ENDFRAME
-
-##########
 read_string:
 .FRAME buffer, index, char, tmp
     arb -4
@@ -3006,62 +2947,6 @@ report_error:
     add [token_line_num], 0, [rb + 1]
     add [token_column_num], 0, [rb]
     call report_error_at_location
-.ENDFRAME
-
-##########
-strcmp:
-.FRAME str1, str2; tmp, char1, char2, index
-    arb -4
-
-    add 0, 0, [rb + index]
-
-strcmp_loop:
-    add [rb + str1], [rb + index], [ip + 1]
-    add [0], 0, [rb + char1]
-
-    add [rb + str2], [rb + index], [ip + 1]
-    add [0], 0, [rb + char2]
-
-    # different characters, we are done
-    eq  [rb + char1], [rb + char2], [rb + tmp]
-    jz  [rb + tmp], strcmp_done
-
-    # same character, is it 0?
-    jz  [rb + char1], strcmp_done
-
-    add [rb + index], 1, [rb + index]
-    jz  0, strcmp_loop
-
-strcmp_done:
-    mul [rb + char2], -1, [rb + tmp]
-    add [rb + char1], [rb + tmp], [rb + tmp]
-
-    arb 4
-    ret 2
-.ENDFRAME
-
-##########
-strcpy:
-.FRAME src, tgt; index, char
-    arb -2
-
-    add 0, 0, [rb + index]
-
-strcpy_loop:
-    add [rb + src], [rb + index], [ip + 1]
-    add [0], 0, [rb + char]
-
-    add [rb + tgt], [rb + index], [ip + 3]
-    add [rb + char], 0, [0]
-
-    jz  [rb + char], strcpy_done
-
-    add [rb + index], 1, [rb + index]
-    jz  0, strcpy_loop
-
-strcpy_done:
-    arb 2
-    ret 2
 .ENDFRAME
 
 ##########
