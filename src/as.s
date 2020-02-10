4e8eb22dc28762aed0ee30953f05a1f28b13d84e
Matus Horvath <matus.horvath@sap.com>
2020-02-10 16:01:39 +0100

    error.s

--- src/as.s
+++ src/as.s
@@ -21,6 +21,9 @@
 .IMPORT print_num
 .IMPORT print_str
 
+# from error.s
+.IMPORT report_error_at_location
+
 ##########
     arb stack
 
@@ -3053,57 +3056,6 @@ report_error:
     add [token_line_num], 0, [rb + 1]
     add [token_column_num], 0, [rb]
     call report_error_at_location
-.ENDFRAME
-
-##########
-report_error_at_location:
-.FRAME message, line_num, column_num;
-    add report_error_msg_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + message], 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add report_error_msg_line, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + line_num], 0, [rb - 1]
-    arb -1
-    call print_num
-
-    add report_error_msg_column, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + column_num], 0, [rb - 1]
-    arb -1
-    call print_num
-
-    add report_error_msg_end, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    out 10
-
-    # instead of halting, we will read all inputs, which hopefully
-    # will cause the intcode vm to crash, indicating a problem
-    # this is the only way we can return a non-zero result from intcode
-
-report_error_loop:
-    in  [0]
-    jz  0, report_error_loop
-
-report_error_msg_start:
-    db "Error: ", 0
-report_error_msg_line:
-    db " (line ", 0
-report_error_msg_column:
-    db ", column ", 0
-report_error_msg_end:
-    db ")", 0
 .ENDFRAME
 
 ##########
