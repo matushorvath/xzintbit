9f07785579435decb79156abf9fea5d88ba00e7f
Matus Horvath <matus.horvath@gmail.com>
2020-09-01 17:33:29 +0200

    Split as global.s

--- src/as/as_split.s
+++ src/as/as_split.s
@@ -8,8 +8,6 @@
 .EXPORT parse
 .EXPORT do_fixups
 
-.EXPORT global_head
-
 # from libxib/heap.s
 .IMPORT alloc
 .IMPORT free
@@ -18,9 +16,12 @@
 .IMPORT strcmp
 .IMPORT strcpy
 
-# from util.s
-.IMPORT report_error
-.IMPORT report_symbol_error
+# from global.s
+.IMPORT find_global_symbol
+.IMPORT add_global_symbol
+.IMPORT set_global_symbol_address
+.IMPORT set_global_symbol_type
+.IMPORT global_head
 
 # from lexer.s
 .IMPORT get_token
@@ -33,6 +34,10 @@
 .IMPORT set_mem
 .IMPORT set_mem_str
 .IMPORT inc_mem_at
+
+# from util.s
+.IMPORT report_error
+.IMPORT report_symbol_error
 
 ##########
 parse:
@@ -834,7 +839,7 @@ parse_dir_frame_block_identifier:
     call find_frame_symbol
     jz  [rb - 3], parse_dir_frame_block_is_unique
 
-    add err_duplicate_symbol, 0, [rb]
+    add err_duplicate_frame_symbol, 0, [rb]
     call report_error
 
 parse_dir_frame_block_is_unique:
@@ -1386,77 +1391,6 @@ parse_number_or_char_have_value:
 .ENDFRAME
 
 ##########
-find_global_symbol:
-.FRAME identifier; record
-    arb -1
-
-    add [global_head], 0, [rb + record]
-
-find_global_symbol_loop:
-    # are there any more records?
-    jz  [rb + record], find_global_symbol_done
-
-    # does this record contain the identifier?
-    add [rb + identifier], 0, [rb - 1]
-    add [rb + record], GLOBAL_IDENTIFIER, [rb - 2]
-    arb -2
-    call strcmp
-
-    # if strcmp result is 0, we are done
-    jz  [rb - 4], find_global_symbol_done
-
-    # move to next record
-    add [rb + record], GLOBAL_NEXT_PTR, [ip + 1]
-    add [0], 0, [rb + record]
-
-    jz  0, find_global_symbol_loop
-
-find_global_symbol_done:
-    arb 1
-    ret 1
-.ENDFRAME
-
-##########
-add_global_symbol:
-.FRAME identifier; record
-    arb -1
-
-    # allocate a block
-    add GLOBAL_SIZE, 0, [rb - 1]
-    arb -1
-    call alloc
-    add [rb - 3], 0, [rb + record]
-
-    # set pointer to next symbol
-    add [rb + record], GLOBAL_NEXT_PTR, [ip + 3]
-    add [global_head], 0, [0]
-
-    # store the identifier
-    add [rb + identifier], 0, [rb - 1]
-    add [rb + record], GLOBAL_IDENTIFIER, [rb - 2]
-    arb -2
-    call strcpy
-
-    # set the symbol as local by default
-    add [rb + record], GLOBAL_TYPE, [ip + 3]
-    add 0, 0, [0]
-
-    # set address to -1, so we can detect when the address is set
-    add [rb + record], GLOBAL_ADDRESS, [ip + 3]
-    add -1, 0, [0]
-
-    # set fixup head to 0
-    add [rb + record], GLOBAL_FIXUPS_HEAD, [ip + 3]
-    add 0, 0, [0]
-
-    # set new symbol head
-    add [rb + record], 0, [global_head]
-
-    arb 1
-    ret 1
-.ENDFRAME
-
-##########
 add_fixup:
 .FRAME identifier, address, line_num, column_num; symbol, fixup, tmp
     arb -3
@@ -1507,105 +1441,6 @@ add_fixup_have_symbol:
 
     arb 3
     ret 4
-.ENDFRAME
-
-##########
-set_global_symbol_address:
-.FRAME identifier, address; symbol, tmp
-    arb -2
-
-    # find or create the symbol record
-    add [rb + identifier], 0, [rb - 1]
-    arb -1
-    call find_global_symbol
-    add [rb - 3], 0, [rb + symbol]
-
-    jnz [rb + symbol], set_global_symbol_address_check_duplicate
-
-    add [rb + identifier], 0, [rb - 1]
-    arb -1
-    call add_global_symbol
-    add [rb - 3], 0, [rb + symbol]
-
-    jz  0, set_global_symbol_address_have_symbol
-
-set_global_symbol_address_check_duplicate:
-    # check for duplicate symbol definitions
-    add [rb + symbol], GLOBAL_ADDRESS, [ip + 1]
-    add [0], 0, [rb + tmp]
-
-    eq  [rb + tmp], -1, [rb + tmp]
-    jnz [rb + tmp], set_global_symbol_address_have_symbol
-
-    add err_duplicate_symbol, 0, [rb]
-    call report_error
-
-set_global_symbol_address_have_symbol:
-    # store the address of the symbol
-    add [rb + symbol], GLOBAL_ADDRESS, [ip + 3]
-    add [rb + address], 0, [0]
-
-    arb 2
-    ret 2
-.ENDFRAME
-
-##########
-set_global_symbol_type:
-.FRAME identifier, type; symbol, tmp
-    arb -2
-
-    # find or create the symbol record
-    add [rb + identifier], 0, [rb - 1]
-    arb -1
-    call find_global_symbol
-    add [rb - 3], 0, [rb + symbol]
-
-    jnz [rb + symbol], set_global_symbol_type_check
-
-    add [rb + identifier], 0, [rb - 1]
-    arb -1
-    call add_global_symbol
-    add [rb - 3], 0, [rb + symbol]
-
-    jz  0, set_global_symbol_type_have_symbol
-
-set_global_symbol_type_check:
-    # check for symbol already imported/exported
-    add [rb + symbol], GLOBAL_TYPE, [ip + 1]
-    add [0], 0, [rb + tmp]
-
-    jz  [rb + tmp], set_global_symbol_type_have_symbol
-
-    eq  [rb + tmp], [rb + type], [rb + tmp]
-    jnz [rb + tmp], set_global_symbol_type_check_same
-
-    add err_symbol_symbol_type_mix, 0, [rb]
-    call report_error
-
-set_global_symbol_type_check_same:
-    eq  [rb + type], 1, [rb + tmp]
-    jnz [rb + tmp], set_global_symbol_type_error_imported
-    eq  [rb + type], 2, [rb + tmp]
-    jnz [rb + tmp], set_global_symbol_type_error_exported
-
-    add err_constant_already_defined, 0, [rb]
-    call report_error
-
-set_global_symbol_type_error_imported:
-    add err_symbol_already_imported, 0, [rb]
-    call report_error
-
-set_global_symbol_type_error_exported:
-    add err_symbol_already_exported, 0, [rb]
-    call report_error
-
-set_global_symbol_type_have_symbol:
-    # set symbol type
-    add [rb + symbol], GLOBAL_TYPE, [ip + 3]
-    add [rb + type], 0, [0]
-
-    arb 2
-    ret 2
 .ENDFRAME
 
 ##########
@@ -1807,10 +1642,6 @@ calc_fixup_done:
 ##########
 # globals
 
-# head of the linked list of global symbols
-global_head:
-    db  0
-
 # head of the linked list of frame symbols
 frame_head:
     db  0
@@ -1866,8 +1697,8 @@ err_expect_number_char_identifier:
     db  "Expecting a number, character or identifier", 0
 err_expect_number_char:
     db  "Expecting a number or character", 0
-err_duplicate_symbol:
-    db  "Duplicate symbol definition", 0
+err_duplicate_frame_symbol:
+    db  "Duplicate frame symbol definition", 0
 err_unknown_symbol:
     db  "Unknown symbol", 0
 err_expect_endframe:
@@ -1876,13 +1707,5 @@ err_global_symbol_not_allowed:
     db  "Global symbol is not allowed here", 0
 err_frame_symbol_not_allowed:
     db  "Frame symbol is not allowed here", 0
-err_symbol_symbol_type_mix:
-    db  "Redefining symbol type is not allowed", 0
-err_symbol_already_imported:
-    db  "Symbol is already imported", 0
-err_symbol_already_exported:
-    db  "Symbol is already exported", 0
-err_constant_already_defined:
-    db  "Constant symbol is already defined", 0
 
 .EOF
---
+++ src/as/global.s
@@ -0,0 +1,208 @@
+.EXPORT find_global_symbol
+.EXPORT add_global_symbol
+.EXPORT set_global_symbol_address
+.EXPORT set_global_symbol_type
+.EXPORT global_head
+
+# from libxib/heap.s
+.IMPORT alloc
+
+# from libxib/string.s
+.IMPORT strcmp
+.IMPORT strcpy
+
+# from util.s
+.IMPORT report_error
+
+##########
+find_global_symbol:
+.FRAME identifier; record
+    arb -1
+
+    add [global_head], 0, [rb + record]
+
+find_global_symbol_loop:
+    # are there any more records?
+    jz  [rb + record], find_global_symbol_done
+
+    # does this record contain the identifier?
+    add [rb + identifier], 0, [rb - 1]
+    add [rb + record], GLOBAL_IDENTIFIER, [rb - 2]
+    arb -2
+    call strcmp
+
+    # if strcmp result is 0, we are done
+    jz  [rb - 4], find_global_symbol_done
+
+    # move to next record
+    add [rb + record], GLOBAL_NEXT_PTR, [ip + 1]
+    add [0], 0, [rb + record]
+
+    jz  0, find_global_symbol_loop
+
+find_global_symbol_done:
+    arb 1
+    ret 1
+.ENDFRAME
+
+##########
+add_global_symbol:
+.FRAME identifier; record
+    arb -1
+
+    # allocate a block
+    add GLOBAL_SIZE, 0, [rb - 1]
+    arb -1
+    call alloc
+    add [rb - 3], 0, [rb + record]
+
+    # set pointer to next symbol
+    add [rb + record], GLOBAL_NEXT_PTR, [ip + 3]
+    add [global_head], 0, [0]
+
+    # store the identifier
+    add [rb + identifier], 0, [rb - 1]
+    add [rb + record], GLOBAL_IDENTIFIER, [rb - 2]
+    arb -2
+    call strcpy
+
+    # set the symbol as local by default
+    add [rb + record], GLOBAL_TYPE, [ip + 3]
+    add 0, 0, [0]
+
+    # set address to -1, so we can detect when the address is set
+    add [rb + record], GLOBAL_ADDRESS, [ip + 3]
+    add -1, 0, [0]
+
+    # set fixup head to 0
+    add [rb + record], GLOBAL_FIXUPS_HEAD, [ip + 3]
+    add 0, 0, [0]
+
+    # set new symbol head
+    add [rb + record], 0, [global_head]
+
+    arb 1
+    ret 1
+.ENDFRAME
+
+##########
+set_global_symbol_address:
+.FRAME identifier, address; symbol, tmp
+    arb -2
+
+    # find or create the symbol record
+    add [rb + identifier], 0, [rb - 1]
+    arb -1
+    call find_global_symbol
+    add [rb - 3], 0, [rb + symbol]
+
+    jnz [rb + symbol], set_global_symbol_address_check_duplicate
+
+    add [rb + identifier], 0, [rb - 1]
+    arb -1
+    call add_global_symbol
+    add [rb - 3], 0, [rb + symbol]
+
+    jz  0, set_global_symbol_address_have_symbol
+
+set_global_symbol_address_check_duplicate:
+    # check for duplicate symbol definitions
+    add [rb + symbol], GLOBAL_ADDRESS, [ip + 1]
+    add [0], 0, [rb + tmp]
+
+    eq  [rb + tmp], -1, [rb + tmp]
+    jnz [rb + tmp], set_global_symbol_address_have_symbol
+
+    add err_duplicate_global_symbol, 0, [rb]
+    call report_error
+
+set_global_symbol_address_have_symbol:
+    # store the address of the symbol
+    add [rb + symbol], GLOBAL_ADDRESS, [ip + 3]
+    add [rb + address], 0, [0]
+
+    arb 2
+    ret 2
+.ENDFRAME
+
+##########
+set_global_symbol_type:
+.FRAME identifier, type; symbol, tmp
+    arb -2
+
+    # find or create the symbol record
+    add [rb + identifier], 0, [rb - 1]
+    arb -1
+    call find_global_symbol
+    add [rb - 3], 0, [rb + symbol]
+
+    jnz [rb + symbol], set_global_symbol_type_check
+
+    add [rb + identifier], 0, [rb - 1]
+    arb -1
+    call add_global_symbol
+    add [rb - 3], 0, [rb + symbol]
+
+    jz  0, set_global_symbol_type_have_symbol
+
+set_global_symbol_type_check:
+    # check for symbol already imported/exported
+    add [rb + symbol], GLOBAL_TYPE, [ip + 1]
+    add [0], 0, [rb + tmp]
+
+    jz  [rb + tmp], set_global_symbol_type_have_symbol
+
+    eq  [rb + tmp], [rb + type], [rb + tmp]
+    jnz [rb + tmp], set_global_symbol_type_check_same
+
+    add err_symbol_symbol_type_mix, 0, [rb]
+    call report_error
+
+set_global_symbol_type_check_same:
+    eq  [rb + type], 1, [rb + tmp]
+    jnz [rb + tmp], set_global_symbol_type_error_imported
+    eq  [rb + type], 2, [rb + tmp]
+    jnz [rb + tmp], set_global_symbol_type_error_exported
+
+    add err_constant_already_defined, 0, [rb]
+    call report_error
+
+set_global_symbol_type_error_imported:
+    add err_symbol_already_imported, 0, [rb]
+    call report_error
+
+set_global_symbol_type_error_exported:
+    add err_symbol_already_exported, 0, [rb]
+    call report_error
+
+set_global_symbol_type_have_symbol:
+    # set symbol type
+    add [rb + symbol], GLOBAL_TYPE, [ip + 3]
+    add [rb + type], 0, [0]
+
+    arb 2
+    ret 2
+.ENDFRAME
+
+##########
+# globals
+
+# head of the linked list of global symbols
+global_head:
+    db  0
+
+##########
+# error messages
+
+err_duplicate_global_symbol:
+    db  "Duplicate global symbol definition", 0
+err_symbol_symbol_type_mix:
+    db  "Redefining symbol type is not allowed", 0
+err_symbol_already_imported:
+    db  "Symbol is already imported", 0
+err_symbol_already_exported:
+    db  "Symbol is already exported", 0
+err_constant_already_defined:
+    db  "Constant symbol is already defined", 0
+
+.EOF
