261ab95f5574b268f82181cbc244e8dec10b6d78
Matus Horvath <matus.horvath@gmail.com>
2020-09-01 17:18:30 +0200

    Split ld util.s

---
+++ src/ld/util.s
@@ -0,0 +1,20 @@
+.EXPORT report_error
+
+# from libxib/error.s
+.IMPORT report_error_at_location
+
+# from ld_split.s
+.IMPORT input_line_num
+.IMPORT input_column_num
+
+##########
+report_error:
+.FRAME message;
+    # we don't bother with updating the stack pointer, this function never returns
+    add [rb + message], 0, [rb + 2]
+    add [input_line_num], 0, [rb + 1]
+    add [input_column_num], 0, [rb]
+    call report_error_at_location
+.ENDFRAME
+
+.EOF
