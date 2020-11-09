04f46de3c36a1c978df86e59da113a29906a5956
Matus Horvath <matus.horvath@gmail.com>
2020-11-09 10:29:14 +0100

    Profile min and max memory value

--- vm/profile.c
+++ vm/profile.c
@@ -5,6 +5,8 @@
 #include "profile.h"
 
 static int max_mem_size = 0;
+static int min_value = 0;
+static int max_value = 0;
 static int inst_count = 0;
 
 void profile_init() {
@@ -18,16 +20,25 @@ void profile_addr(int addr) {
     }
 }
 
+void profile_value(int value) {
+    if (value > max_value) {
+        max_value = value;
+    }
+    if (value < min_value) {
+        min_value = value;
+    }
+}
+
 void profile_inst() {
     inst_count++;
 }
 
 void profile_done() {
-    fprintf(stderr, "profile: max_mem_size = %d inst_count = %d\n",
-        max_mem_size, inst_count);
+    fprintf(stderr, "profile: max_mem_size = %d inst_count = %d min_value = %d max_value = %d\n",
+        max_mem_size, inst_count, min_value, max_value);
 
     FILE* fp = fopen("icvm_profile.csv", "at");
-    fprintf(fp, "%d, %d\n", max_mem_size, inst_count);
+    fprintf(fp, "%d, %d, %d, %d\n", max_mem_size, inst_count, min_value, max_value);
     fclose(fp);
 }
 
