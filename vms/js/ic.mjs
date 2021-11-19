1be634d595833f0428c398eb1d4e645bdb0139db
Matus Horvath <matus.horvath@gmail.com>
2021-11-19 16:47:26 +0100

    Remove recursion from JS VM

--- vms/js/ic.mjs
+++ vms/js/ic.mjs
@@ -3,20 +3,27 @@
 import fs from 'fs';
 import fsp from 'fs/promises';
 
+// Node.js does not have tail call optimization, so we can't use recursion instead of loops
+const whilef = async (f, c, s) => {
+    while (c(s)) s = await f(s);
+    return s;
+}
+
 const main = async (path) => String.fromCharCode(...(await run({ mem: await load(path), ip: 0, rb: 0, out: [] })));
 const load = async (path) => (await fsp.readFile(path, 'utf8')).split(',').map(n => parseInt(n, 10));
-const run = async (s) => ({ 1: add, 2: mul, 3: inn, 4: out, 5: jnz, 6: jz, 7: lt, 8: eq, 9: arb, 99: hlt })[getmem(s.mem, s.ip) % 100](s);
+const decode = (s) => ({ 1: add, 2: mul, 3: inn, 4: out, 5: jnz, 6: jz, 7: lt, 8: eq, 9: arb, 99: hlt })[getmem(s.mem, s.ip) % 100];
+const run = async (s) => (await whilef(async s => await decode(s)(s), s => !s.stop, s)).out;
 
-const add = async (s) => run({ ...s, mem: setparam(2, s, getparam(0, s) + getparam(1, s)), ip: s.ip + 4 });
-const mul = async (s) => run({ ...s, mem: setparam(2, s, getparam(0, s) * getparam(1, s)), ip: s.ip + 4 });
-const inn = async (s) => (v => v !== undefined ? run({ ...s, mem: setparam(0, s, v), ip: s.ip + 2 }) : s.out)(await getinput());
-const out = async (s) => run({ ...s, out: [...s.out, getparam(0, s)], ip: s.ip + 2 });
-const jnz = async (s) => run({ ...s, ip: getparam(0, s) !== 0 ? getparam(1, s) : s.ip + 3 });
-const jz  = async (s) => run({ ...s, ip: getparam(0, s) === 0 ? getparam(1, s) : s.ip + 3 });
-const lt  = async (s) => run({ ...s, mem: setparam(2, s, getparam(0, s) < getparam(1, s) ? 1 : 0), ip: s.ip + 4 });
-const eq  = async (s) => run({ ...s, mem: setparam(2, s, getparam(0, s) === getparam(1, s) ? 1 : 0), ip: s.ip + 4 });
-const arb = async (s) => run({ ...s, rb: s.rb + getparam(0, s), ip: s.ip + 2 });
-const hlt = async (s) => s.out;
+const add = async (s) => ({ ...s, mem: setparam(2, s, getparam(0, s) + getparam(1, s)), ip: s.ip + 4 });
+const mul = async (s) => ({ ...s, mem: setparam(2, s, getparam(0, s) * getparam(1, s)), ip: s.ip + 4 });
+const inn = async (s) => (v => ({ ...s, mem: setparam(0, s, v), ip: s.ip + 2, stop: v === undefined }))(await getinput());
+const out = async (s) => ({ ...s, out: [...s.out, getparam(0, s)], ip: s.ip + 2 });
+const jnz = async (s) => ({ ...s, ip: getparam(0, s) !== 0 ? getparam(1, s) : s.ip + 3 });
+const jz  = async (s) => ({ ...s, ip: getparam(0, s) === 0 ? getparam(1, s) : s.ip + 3 });
+const lt  = async (s) => ({ ...s, mem: setparam(2, s, getparam(0, s) < getparam(1, s) ? 1 : 0), ip: s.ip + 4 });
+const eq  = async (s) => ({ ...s, mem: setparam(2, s, getparam(0, s) === getparam(1, s) ? 1 : 0), ip: s.ip + 4 });
+const arb = async (s) => ({ ...s, rb: s.rb + getparam(0, s), ip: s.ip + 2 })
+const hlt = async (s) => ({ ...s, stop: true });
 
 const getparam = (i, s) => [getparamp, getparami, getparamr][Math.trunc(getmem(s.mem, s.ip) / (10 ** (i + 2))) % 10](i, s);
 const getparamp = (i, s) => getmem(s.mem, getmem(s.mem, s.ip + i + 1));
