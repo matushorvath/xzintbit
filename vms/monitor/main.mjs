00fb7d85d7f9476add1560d0f4bfc6420d18a725
Matus Horvath <matus.horvath@gmail.com>
2024-01-28 06:43:43 +0100

    Add express and p5

--- vms/monitor/main.mjs
+++ vms/monitor/main.mjs
@@ -1,14 +1,24 @@
 /* eslint-env node */
 
 import { app, BrowserWindow } from 'electron';
+import express from 'express';
 
 import path from 'node:path';
-import timers from 'node:timers/promises';
-import url from 'url';
+import url from 'node:url';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const createWindow = () => {
+const onEventReceived = async (req, res, win) => {
+    console.log(req.body);
+    win.webContents.send('update-state', req.body);
+    res.status(201).send('Created');
+};
+
+const server = express();
+const port = 2019;
+server.use(express.json());
+
+const onElectronReady = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -17,29 +27,15 @@ const createWindow = () => {
         }
     });
 
+    server.post('/event', (req, res) => onEventReceived(req, res, win));
+    server.listen(port, () => console.log(`Listening on port ${port}`));
+
+    // TODO race condition, we must not receive events until the browser part is ready for them
+    // TODO probably handle by maintaining state in node, and exposing the state to renderer
+    // TODO or synchronize the startup, make browser code send and event to us that it is ready
+
     win.loadFile('index.html');
     return win;
 };
 
-const updateState = (win, update) => {
-    console.log(update);
-    win.webContents.send('update-state', update);
-};
-
-const main = async () => {
-    await app.whenReady();
-    console.log('ready');
-
-    const win = createWindow();
-
-    let counter = 0;
-    while (true) {
-        await timers.setTimeout(1000);
-        updateState(win, counter++);
-    }
-};
-
-main().catch(e => {
-    console.log(e);
-    app.quit();
-});
+app.whenReady().then(() => onElectronReady());
--- vms/monitor/renderer.mjs
+++ vms/monitor/renderer.mjs
@@ -4,8 +4,42 @@ const stateElement = document.getElementById('state');
 
 const onUpdateState = (update) => {
     console.log(update);
-    stateElement.innerText = update;
+    stateElement.innerText = JSON.stringify(update);
 };
 
 window.ipc.onUpdateState(onUpdateState);
-console.log('registered');
+
+let coswave = [];
+
+function setup() {
+    createCanvas(720, 360);
+    for (let i = 0; i < width; i++) {
+        let amount = map(i, 0, width, 0, PI);
+        coswave[i] = abs(cos(amount));
+    }
+    background(255);
+    noLoop();
+}
+
+function draw() {
+    let y1 = 0;
+    let y2 = height / 3;
+    for (let i = 0; i < width; i += 3) {
+        stroke(coswave[i] * 255);
+        line(i, y1, i, y2);
+    }
+
+    y1 = y2;
+    y2 = y1 + y1;
+    for (let i = 0; i < width; i += 3) {
+        stroke((coswave[i] * 255) / 4);
+        line(i, y1, i, y2);
+    }
+
+    y1 = y2;
+    y2 = height;
+    for (let i = 0; i < width; i += 3) {
+        stroke(255 - coswave[i] * 255);
+        line(i, y1, i, y2);
+    }
+}
