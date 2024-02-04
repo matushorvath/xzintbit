59fc9d4d6668611e512275f2a4f35c46cbab7f1c
Matus Horvath <matus.horvath@gmail.com>
2024-02-04 19:04:26 +0100

    Refactor js vm

--- vms/js/ic.mjs
+++ vms/js/ic.mjs
@@ -1,109 +1,5 @@
 import fs from 'fs/promises';
-
-let mem;
-
-let ip = 0;
-let rb = 0;
-
-const getMem = (addr) => {
-    return mem[addr] ?? 0;
-};
-
-const setMem = (addr, val) => {
-    mem[addr] = val;
-};
-
-const MODE_MUL = [100, 1000, 10000];
-
-const getParam = (idx) => {
-    const mode = Math.floor(getMem(ip) / MODE_MUL[idx]) % 10;
-    switch (mode) {
-        case 0: // position mode
-            return getMem(getMem(ip + idx + 1));
-        case 1: // immediate mode
-            return getMem(ip + idx + 1);
-        case 2: // relative mode
-            return getMem(rb + getMem(ip + idx + 1));
-        default:
-            throw new Error(`mode error: ip ${ip} idx ${idx}`);
-    }
-}
-
-const setParam = (idx, val) => {
-    const mode = Math.floor(getMem(ip) / MODE_MUL[idx]) % 10;
-    switch (mode) {
-        case 0: // position mode
-            setMem(getMem(ip + idx + 1), val);
-            break;
-        case 2: // relative mode
-            setMem(rb + getMem(ip + idx + 1), val);
-            break;
-        default:
-            throw new Error(`mode error: ip ${ip} idx ${idx}`);
-    }
-}
-
-const run = async function* (ins = (async function* () {})()) {
-    while (true) {
-        const oc = Math.floor(getMem(ip) % 100);
-
-        switch (oc) {
-            case 1: // add
-                setParam(2, getParam(0) + getParam(1));
-                ip += 4;
-                break;
-            case 2: // mul
-                setParam(2, getParam(0) * getParam(1));
-                ip += 4;
-                break;
-            case 3: { // in
-                const { value, done } = await ins.next();
-                if (done) {
-                    throw new Error('no more inputs');
-                }
-                setParam(0, value);
-                ip += 2;
-                break;
-            }
-            case 4: { // out
-                const value = getParam(0);
-                ip += 2;
-                yield value;
-                break;
-            }
-            case 5: // jnz
-                if (getParam(0) !== 0) {
-                    ip = getParam(1);
-                } else {
-                    ip += 3;
-                }
-                break;
-            case 6: // jz
-                if (getParam(0) === 0) {
-                    ip = getParam(1);
-                } else {
-                    ip += 3;
-                }
-                break;
-            case 7: // lt
-                setParam(2, getParam(0) < getParam(1) ? 1 : 0);
-                ip += 4;
-                break;
-            case 8: // eq
-                setParam(2, getParam(0) === getParam(1) ? 1 : 0);
-                ip += 4;
-                break;
-            case 9: // arb
-                rb += getParam(0);
-                ip += 2;
-                break;
-            case 99: // hlt
-                return;
-            default:
-                throw new Error(`opcode error: ip ${ip} oc ${oc}`);
-        }
-    }
-}
+import { Vm } from './vm.mjs';
 
 async function* getIns() {
     for await (const chunk of process.stdin) {
@@ -115,9 +11,11 @@ async function* getIns() {
 
 const main = async () => {
     const input = await fs.readFile(process.argv[2], 'utf8');
-    mem = input.split(',').map(i => Number(i));
+    const mem = input.split(',').map(i => Number(i));
 
-    for await (const char of run(getIns())) {
+    const vm = new Vm();
+
+    for await (const char of vm.run(mem, getIns())) {
         process.stdout.write(String.fromCharCode(char));
     }
 };
---
+++ vms/js/vm.mjs
@@ -0,0 +1,110 @@
+export class Vm {
+    mem = [];
+
+    ip = 0;
+    rb = 0;
+
+    getMem(addr) {
+        return this.mem[addr] ?? 0;
+    }
+
+    setMem(addr, val) {
+        this.mem[addr] = val;
+    }
+
+    static MODE_MUL = [100, 1000, 10000];
+
+    getParam(idx) {
+        const mode = Math.floor(this.getMem(this.ip) / Vm.MODE_MUL[idx]) % 10;
+        switch (mode) {
+            case 0: // position mode
+                return this.getMem(this.getMem(this.ip + idx + 1));
+            case 1: // immediate mode
+                return this.getMem(this.ip + idx + 1);
+            case 2: // relative mode
+                return this.getMem(this.rb + this.getMem(this.ip + idx + 1));
+            default:
+                throw new Error(`mode error: ip ${this.ip} idx ${idx}`);
+        }
+    }
+
+    setParam(idx, val) {
+        const mode = Math.floor(this.getMem(this.ip) / Vm.MODE_MUL[idx]) % 10;
+        switch (mode) {
+            case 0: // position mode
+                this.setMem(this.getMem(this.ip + idx + 1), val);
+                break;
+            case 2: // relative mode
+                this.setMem(this.rb + this.getMem(this.ip + idx + 1), val);
+                break;
+            default:
+                throw new Error(`mode error: ip ${this.ip} idx ${idx}`);
+        }
+    }
+
+    async* run(mem, ins = (async function* () {})()) {
+        this.ip = this.rb = 0;
+        this.mem = mem;
+        this.events = {};
+
+        while (true) {
+            const oc = Math.floor(this.getMem(this.ip) % 100);
+
+            switch (oc) {
+                case 1: // add
+                    this.setParam(2, this.getParam(0) + this.getParam(1));
+                    this.ip += 4;
+                    break;
+                case 2: // mul
+                    this.setParam(2, this.getParam(0) * this.getParam(1));
+                    this.ip += 4;
+                    break;
+                case 3: { // in
+                    const { value, done } = await ins.next();
+                    if (done) {
+                        throw new Error('no more inputs');
+                    }
+                    this.setParam(0, value);
+                    this.ip += 2;
+                    break;
+                }
+                case 4: { // out
+                    const value = this.getParam(0);
+                    this.ip += 2;
+                    yield value;
+                    break;
+                }
+                case 5: // jnz
+                    if (this.getParam(0) !== 0) {
+                        this.ip = this.getParam(1);
+                    } else {
+                        this.ip += 3;
+                    }
+                    break;
+                case 6: // jz
+                    if (this.getParam(0) === 0) {
+                        this.ip = this.getParam(1);
+                    } else {
+                        this.ip += 3;
+                    }
+                    break;
+                case 7: // lt
+                    this.setParam(2, this.getParam(0) < this.getParam(1) ? 1 : 0);
+                    this.ip += 4;
+                    break;
+                case 8: // eq
+                    this.setParam(2, this.getParam(0) === this.getParam(1) ? 1 : 0);
+                    this.ip += 4;
+                    break;
+                case 9: // arb
+                    this.rb += this.getParam(0);
+                    this.ip += 2;
+                    break;
+                case 99: // hlt
+                    return;
+                default:
+                    throw new Error(`opcode error: ip ${this.ip} oc ${oc}`);
+            }
+        }
+    }
+};
