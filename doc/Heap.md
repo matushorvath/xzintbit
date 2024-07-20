d3609b5a49ef19215ed5096e3bb899259f339171
Matus Horvath <matus.horvath@gmail.com>
2024-07-20 08:41:30 +0200

    New allocator, 8-byte aligned, WIP

---
+++ doc/Heap.md
@@ -0,0 +1,91 @@
+alloc:
+
+as:
+FIXUP_SIZE      4
+FRAME_SIZE      50
+    has identifier, strcpy
+GLOBAL_SIZE     50
+    has identifier, strcpy
+
+ld:
+MODULE_SIZE     13
+EXPORT_SIZE     7
+IMPORT_SIZE     7
+
+libxib:
+MEM_BLOCK_SIZE  50
+    lexer.s: read_identifier, read_string
+    memory.s: set_mem
+
+-----
+
+free:
+
+as:
+reset_frame                     free frame symbols when exiting frame - free identifier
+read_identifier_or_keyword      free string after detecting a keyword
+read_directive                  free string after detecting a directive
+parse_db                        free string after setting it to memory - check set_as_mem_str parameter length
+parse_dir_frame_block           free string after copying it to frame symbol - check add_frame_symbol identifier length - instead hand over the string
+parse_value                     free string after copying it to a fixup - check add_fixup, token_value parameter (1st) - instead hand over the string
+parse_symbol                    free string after copying it to global symbol - check set_global_symbol_address, token_value (1st) - instead hand over the string
+parse_dir_symbol                free identifier (its 2nd param) - check set_global_symbol_address as above, check all callers, they will be handing over the string
+                                there is something complex going on with parse_dir_symbol, it returns the identifier to caller, but only sometimes...
+parse_dir_import_export         free string after copying it to global symbol - check set_global_symbol_type
+
+ld:
+load_imported                   loaded identifier from .o, gets freed after processing
+load_exported                   same as load_imported
+
+all strings we now hand over, check where they come from
+[token_value] is a global!
+
+-----
+
+alloc always allocates MEM_BLOCK_SIZE = 50, fails for bigger allocations
+
+https://sourceware.org/glibc/wiki/MallocInternals
+
+-----
+
+Design:
+
+chunk:
+    size
+    free
+    ----
+    next
+    prev
+
+min chunk size: >2, say 8
+
+small:
+    8
+    16
+    32
+    64
+
+    table request size -> bin to avoid division
+
+large:
+    >64, probably just one list
+
+alloc algorithm:
+    if (size > 64) alloc_large
+    else alloc_small
+
+alloc_small:
+    request_size = size rounded up to nearest power of min chunk size = 8, calculate using a table
+    go through the respective list
+    return first item from the list, relink
+    if not found, go through larger small lists, first item, cut it, return and store the rest in correct small list
+    if not found, call alloc_large (but we already have request_size so optimize and reuse it)
+
+alloc_large:
+    request_size = size rounded up to nearest power of min chunk size = 8
+        TODO how to calculate, do we actually divide?
+    iterate through list
+    if chunk_size >= request_size, cut chunk to request_size + rest, save rest to list, return request_sized chunk, relink list
+    else if next chunk is free, merge chunks
+    if end of list, optional go through the small lists and merge them, if large enough then cut and use, else store into large list if >64, small list if <=64
+    if still not found, call sbrk to make new chunk (nearest multiple of 8 sized)
--- src/libxib/heap.s
+++ src/libxib/heap.s
@@ -1,21 +1,275 @@
-# TODO allocate less than MEM_BLOCK_SIZE
-
 .EXPORT alloc
 .EXPORT free
+
+# from brk.s
+.IMPORT sbrk
 
 # from builtin
 .IMPORT __heap_start
 
-# from print.s
-.IMPORT print_str
-
-# from outside of this library
-.IMPORT report_libxib_error
+# TODO logging for alloc/free
 
 ##########
 alloc:
-.FRAME size; block, tmp
+.FRAME size; ptr, tmp                   # returns ptr
+    arb -X
+
+    # parameter validation
+    lt  [rb + size], 1, [rb + tmp]
+    jz  [rb + tmp], alloc_size_ok
+
+    add 0, 0, [rb + ptr]
+    jz  0, alloc_done
+
+alloc_size_ok:
+    # the chunk will use 2 additional bytes for the chunk header
+    add [rb + size], 2, [rb + size]
+
+    # select the large allocator for requests >64 bytes
+    lt  64, [rb + size], [rb + tmp]
+    jnz [rb + tmp], alloc_large_calc_size
+
+    # allocate a small chunk, size rounded up to nearest multiple of 8
+    add multiple_8, [rb + size], [ip + 1]
+    add [0], 0, [rb + size]
+
+    # start from the smallest bin that fits
+    add alloc_small_table, [rb + size], [ip + 2]
+    jz  0, [0]
+
+alloc_small_table:
+    db  0
+    ds  8, alloc_small_8
+    ds  8, alloc_small_16
+    ds  8, alloc_small_24
+    ds  8, alloc_small_32
+    ds  8, alloc_small_40
+    ds  8, alloc_small_48
+    ds  8, alloc_small_56
+    ds  8, alloc_small_64
+
+alloc_small_8:
+    jz  [small_next + 0], alloc_small_16
+
+    # use the first chunk from this bin
+    add [small_next + 0], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 0], 2, [ip + 1]
+    add [0], 0, [small_next + 0]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 0], alloc_cut_chunk
+    add [small_next + 0], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_16:
+    jz  [small_next + 1], alloc_small_24
+
+    # use the first chunk from this bin
+    add [small_next + 1], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 1], 2, [ip + 1]
+    add [0], 0, [small_next + 1]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 1], alloc_cut_chunk
+    add [small_next + 1], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_24:
+    jz  [small_next + 2], alloc_small_32
+
+    # use the first chunk from this bin
+    add [small_next + 2], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 2], 2, [ip + 1]
+    add [0], 0, [small_next + 2]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 2], alloc_cut_chunk
+    add [small_next + 2], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_32:
+    jz  [small_next + 3], alloc_small_40
+
+    # use the first chunk from this bin
+    add [small_next + 3], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 3], 2, [ip + 1]
+    add [0], 0, [small_next + 3]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 3], alloc_cut_chunk
+    add [small_next + 3], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_40:
+    jz  [small_next + 4], alloc_small_48
+
+    # use the first chunk from this bin
+    add [small_next + 4], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 4], 2, [ip + 1]
+    add [0], 0, [small_next + 4]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 4], alloc_cut_chunk
+    add [small_next + 4], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_48:
+    jz  [small_next + 5], alloc_small_56
+
+    # use the first chunk from this bin
+    add [small_next + 5], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 5], 2, [ip + 1]
+    add [0], 0, [small_next + 5]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 5], alloc_cut_chunk
+    add [small_next + 5], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_56:
+    jz  [small_next + 6], alloc_small_64
+
+    # use the first chunk from this bin
+    add [small_next + 6], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 6], 2, [ip + 1]
+    add [0], 0, [small_next + 6]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 6], alloc_cut_chunk
+    add [small_next + 6], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_small_64:
+    jz  [small_next + 7], alloc_large
+
+    # use the first chunk from this bin
+    add [small_next + 7], 2, [rb + ptr]
+
+    # second chunk is now head of the list
+    add [small_next + 7], 2, [ip + 1]
+    add [0], 0, [small_next + 7]
+
+    # previous chunk of the new head chunk, if any, is now empty
+    jz  [small_next + 7], alloc_cut_chunk
+    add [small_next + 7], 3, [ip + 3]
+    add 0, 0, [0]
+
+    jz  0, alloc_cut_chunk
+
+alloc_large_calc_size:
+    # allocate a large chunk, size rounded up to nearest multiple of 8
+    add [rb + size], 0, [rb - 1]
+    arb -1
+    call calc_multiple_8
+    add [rb - 3], 0, [rb + size]
+
+alloc_large:
+    # first try to find a fitting chunk in the large bin
+    # TODO also needs size
+    add large_next, 0, [rb - 1]
+    add large_prev, 0, [rb - 2]
     arb -2
+    call alloc_large_bin
+    jz  [rb - 3], alloc_merge_small
+
+    add [rb - 3], 2, [rb + ptr]
+    jz  0, alloc_cut_chunk
+
+alloc_merge_small:
+    # now try to merge the small bins until we build a chunk that fits
+    # TODO also needs size
+    add large_next, 0, [rb - 1]
+    add large_prev, 0, [rb - 2]
+    arb -2
+    call merge_small_bins
+    jz  [rb - 3], alloc_sbrk
+
+    add [rb - 3], 2, [rb + ptr]
+    jz  0, alloc_cut_chunk
+
+alloc_sbrk:
+    # last resort, call sbrk to enlarge the heap
+    add [rb + size], 0, [rb - 1]
+    arb -1
+    call sbrk
+
+    # create a header for the new chunk
+    add [rb - 3], 0, [ip + 3]
+    add [rb + size], 0, [0]
+    add [rb - 3], 1, [ip + 3]
+    add 0, 0, [0]
+
+    # no need to cut this chunk, just return it
+    add [rb - 3], 2, [rb + ptr]
+    jz  0, alloc_done
+
+alloc_cut_chunk:
+    # the chunk pointed to by [rb + ptr] - 2 needs to be split into size and the rest
+    # the first part will be returned in [rb + ptr], the rest if any needs to be stored in the correct bin
+
+    # TODO
+
+alloc_done:
+    arb X
+    ret 1
+.ENDFRAME
+
+##########
+calc_multiple_8:
+.FRAME input; output                    # returns output
+    arb -X
+
+    # TODO
+
+n = 8a + b
+k = 8a - b
+
+n - k = 2b
+
+
+    arb X
+    ret 1
+.ENDFRAME
+
+
+
+alloc_large_bin
+# TODO iterate whole list, merge chunks
+
+merge_small_bins
+# TODO each small bin, merge until you find a matching size, if smaller than needed store in correct bin
+
+
+.EOF
+
 
     # we only support certain block sizes
     lt  MEM_BLOCK_SIZE, [rb + size], [rb + tmp]
@@ -65,18 +319,42 @@ free:
 ##########
 # globals
 
-# head of the linked list of free blocks
-free_head:
+# small bins, for sizes 8, 16, 24, 32, 40, 48, 56 and 64, each is a doubly linked list
+small_next:
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+small_prev:
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
+    db  0
     db  0
 
-# start of unused memory
-heap_end:
-    db  __heap_start
+# large bin, for sizes >64, a doubly linked list
+large_next:
+    db  0
+small_next:
+    db  0
 
-##########
-# error messages
-
-err_allocation_size:
-    db  "Unsupported allocation size", 0
+# table of nearest multiples of 8
+multiple_8:
+    db   0
+    db   8,  8,  8,  8,  8,  8,  8,  8
+    db  16, 16, 16, 16, 16, 16, 16, 16
+    db  24, 24, 24, 24, 24, 24, 24, 24
+    db  32, 32, 32, 32, 32, 32, 32, 32
+    db  40, 40, 40, 40, 40, 40, 40, 40
+    db  48, 48, 48, 48, 48, 48, 48, 48
+    db  56, 56, 56, 56, 56, 56, 56, 56
+    db  64, 64, 64, 64, 64, 64, 64, 64
 
 .EOF
