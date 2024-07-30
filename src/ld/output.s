b3895d7c1aa122580a2b974dce345d8c11b1271d
Matus Horvath <matus.horvath@gmail.com>
2024-07-30 20:19:05 +0200

    New algorithm for map generation

--- src/ld/output.s
+++ src/ld/output.s
@@ -13,8 +13,6 @@
 .IMPORT module_head
 .IMPORT resolved_head
 
-# TODO if an exported symbol is also referenced in the same file, will that be in the map?
-
 ##########
 print_modules:
 .FRAME module, first, tmp
@@ -62,240 +60,6 @@ print_modules:
 
 ##########
 print_map:
-.FRAME
-    call dump_modules
-
-    add .symbols_str, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    call dump_resolved
-    call dump_symbols
-
-    ret 0
-
-.symbols_str:
-    db  "symbols:", 10, 0
-.ENDFRAME
-
-##########
-dump_modules:
-.FRAME module, count
-    arb -2
-
-    add .str_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [module_head], 0, [rb + module]
-    jz  [rb + module], .done
-
-    add .str_head, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    # add a line end after each N modules
-    add [.num_modules_on_line], 0, [rb + count]
-
-.loop:
-    # print module address
-    add [rb + module], MODULE_ADDRESS, [ip + 1]
-    add [0], 0, [rb - 1]
-    arb -1
-    call print_num
-
-.next:
-    # advance to next included module
-    add [rb + module], MODULE_NEXT_PTR, [ip + 1]
-    add [0], 0, [rb + module]
-
-    jz  [rb + module], .done
-
-    add [rb + module], MODULE_INCLUDED, [ip + 1]
-    jz  [0], .next
-
-    # more modules, print separator
-    add [rb + count], -1, [rb + count]
-    jz  [rb + count], .end_line
-
-    add .str_space_separator, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    jz  0, .loop
-
-.end_line:
-    add .str_line_separator, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [.num_modules_on_line], 0, [rb + count]
-
-    jz  0, .loop
-
-.done:
-    add .str_end, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    arb 2
-    ret 0
-
-.num_modules_on_line:
-    db  10
-.str_start:
-    db  "modules:", 0
-.str_head:
-    db  " [", 10, "  ", 0
-.str_space_separator:
-    db  ", ", 0
-.str_line_separator:
-    db  ",", 10, "  ", 0
-.str_end:
-    db  10, "]", 10, 0
-.ENDFRAME
-
-##########
-dump_resolved:
-.FRAME symbol, import, module
-    arb -3
-
-    add [resolved_head], 0, [rb + symbol]
-
-.loop:
-    jz  [rb + symbol], .done
-
-    # get the module for this symbol
-    add [rb + symbol], EXPORT_MODULE, [ip + 1]
-    add [0], 0, [rb + module]
-
-    # skip exports from excluded modules
-    add [rb + module], MODULE_INCLUDED, [ip + 1]
-    jz  [0], .next_export
-
-    # print symbol identifier
-    add .str_identifier_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + symbol], EXPORT_IDENTIFIER, [ip + 1]
-    add [0], 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add .str_identifier_end, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    # print export
-    add .str_export_module_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + module], MODULE_ADDRESS, [ip + 1]
-    add [0], 0, [rb - 1]
-    arb -1
-    call print_num
-
-    add .str_export_offset_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + symbol], EXPORT_ADDRESS, [ip + 1]
-    add [0], 0, [rb - 1]
-    arb -1
-    call print_num
-
-    add .str_export_end, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + symbol], EXPORT_IMPORTS_HEAD, [ip + 1]
-    add [0], 0, [rb + import]
-
-    # print array of imports header
-    add .str_imports_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-.imports_loop:
-    jz  [rb + import], .next_export
-
-    # get the module for this import
-    add [rb + import], IMPORT_MODULE, [ip + 1]
-    add [0], 0, [rb + module]
-
-    # skip imports into excluded modules
-    add [rb + module], MODULE_INCLUDED, [ip + 1]
-    jz  [0], .next_import
-
-    # print array of imports
-    add .str_import_module_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + module], MODULE_ADDRESS, [ip + 1]
-    add [0], 0, [rb - 1]
-    arb -1
-    call print_num
-
-    add .str_import_offsets_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
-    add [rb + import], IMPORT_FIXUPS_HEAD, [ip + 1]
-    add [0], 0, [rb - 1]
-    add [rb + import], IMPORT_FIXUPS_TAIL, [ip + 1]
-    add [0], 0, [rb - 2]
-    add [rb + import], IMPORT_FIXUPS_INDEX, [ip + 1]
-    add [0], 0, [rb - 3]
-    arb -3
-    call pretty_print_mem
-
-    add .str_import_end, 0, [rb - 1]
-    arb -1
-    call print_str
-
-.next_import:
-    add [rb + import], IMPORT_NEXT_PTR, [ip + 1]
-    add [0], 0, [rb + import]
-
-    jz  0, .imports_loop
-
-.next_export:
-    add [rb + symbol], EXPORT_NEXT_PTR, [ip + 1]
-    add [0], 0, [rb + symbol]
-
-    jz  0, .loop
-
-.done:
-    arb 3
-    ret 0
-
-.str_identifier_start:
-    db  "  ", 0
-.str_identifier_end:
-    db  ":", 10, 0
-
-.str_export_module_start:
-    db  "    export:", 10, "      module: ", 0
-.str_export_offset_start:
-    db  10, "      offset: ", 0
-.str_export_end:
-    db  10, 0
-
-.str_imports_start:
-    db  "    imports:", 10, 0
-.str_import_module_start:
-    db  "      - module: ", 0
-.str_import_offsets_start:
-    db  10, "        offsets: [", 0
-.str_import_end:
-    db  "]", 10, 0
-.ENDFRAME
-
-##########
-dump_symbols:
 .FRAME module
     arb -1
 
@@ -304,18 +68,19 @@ dump_symbols:
 .loop:
     jz  [rb + module], .done
 
+    # skip excluded modules
+    add [rb + module], MODULE_INCLUDED, [ip + 1]
+    jz  [0], .next
+
     # dump all symbols within the module
     add [rb + module], 0, [rb - 1]
     arb -1
-    call dump_module_symbols
+    call print_map_symbols
 
 .next:
-    # advance to next included module
+    # advance to next module
     add [rb + module], MODULE_NEXT_PTR, [ip + 1]
     add [0], 0, [rb + module]
-
-    add [rb + module], MODULE_INCLUDED, [ip + 1]
-    jz  [0], .next
 
     jz  0, .loop
 
@@ -325,9 +90,9 @@ dump_symbols:
 .ENDFRAME
 
 ##########
-dump_module_symbols:
-.FRAME module; symbol, reference
-    arb -2
+print_map_symbols:
+.FRAME module; symbol
+    arb -1
 
     add [rb + module], MODULE_SYMBOLS_HEAD, [ip + 1]
     add [0], 0, [rb + symbol]
@@ -336,10 +101,6 @@ dump_module_symbols:
     jz  [rb + symbol], .done
 
     # print the parent identifier
-    add .str_identifier_start, 0, [rb - 1]
-    arb -1
-    call print_str
-
     add [rb + symbol], SYMBOL_PARENT_IDENTIFIER, [ip + 1]
     add [0], 0, [rb - 1]
     arb -1
@@ -347,9 +108,7 @@ dump_module_symbols:
 
     # print the child identifier, if present
     add [rb + symbol], SYMBOL_CHILD_IDENTIFIER, [ip + 1]
-    add [0], 0, [rb - 1]
-
-    jz  [rb - 1], .after_child
+    jz  [0], .after_child
 
     out '.'
 
@@ -359,12 +118,11 @@ dump_module_symbols:
     call print_str
 
 .after_child:
-    add .str_identifier_end, 0, [rb - 1]
-    arb -1
-    call print_str
+    out ':'
+    out 10
 
-    # print symbol
-    add .str_symbol_module_start, 0, [rb - 1]
+    # print the module
+    add .str_module, 0, [rb - 1]
     arb -1
     call print_str
 
@@ -373,25 +131,28 @@ dump_module_symbols:
     arb -1
     call print_num
 
-    add .str_symbol_offset_start, 0, [rb - 1]
+    out 10
+
+    # print the absolute address
+    add .str_address, 0, [rb - 1]
     arb -1
     call print_str
 
-    add [rb + symbol], SYMBOL_ADDRESS, [ip + 1]
-    add [0], 0, [rb - 1]
+    add [rb + module], MODULE_ADDRESS, [ip + 5]
+    add [rb + symbol], SYMBOL_ADDRESS, [ip + 2]
+    add [0], [0], [rb - 1]
     arb -1
     call print_num
 
-    add .str_symbol_end, 0, [rb - 1]
+    out 10
+
+    # print the references
+    add .str_references, 0, [rb - 1]
     arb -1
     call print_str
 
-    # print array of references header
-    add .str_references_offsets_start, 0, [rb - 1]
-    arb -1
-    call print_str
+    out '['
 
-    # print array of references
     add [rb + symbol], SYMBOL_FIXUPS_HEAD, [ip + 1]
     add [0], 0, [rb - 1]
     add [rb + symbol], SYMBOL_FIXUPS_TAIL, [ip + 1]
@@ -401,37 +162,26 @@ dump_module_symbols:
     arb -3
     call pretty_print_mem
 
-    add .str_references_end, 0, [rb - 1]
-    arb -1
-    call print_str
+    # TODO include imported locations as well
 
-.next_symbol:
+    out ']'
+    out 10
+
     add [rb + symbol], SYMBOL_NEXT_PTR, [ip + 1]
     add [0], 0, [rb + symbol]
 
     jz  0, .loop
 
 .done:
-    arb 2
+    arb 1
     ret 1
 
-.str_identifier_start:
-    db  "  ", 0
-.str_identifier_end:
-    db  ":", 10, 0
-
-.str_symbol_module_start:
-    db  "    symbol:", 10, "      module: ", 0
-.str_symbol_offset_start:
-    db  10, "      offset: ", 0
-.str_symbol_end:
-    db  10, 0
-
-.str_references_offsets_start:
-    db  "    references:", 10
-    db  "      - offsets: [", 0
-.str_references_end:
-    db  "]", 10, 0
+.str_module:
+    db  "  module: ", 0
+.str_address:
+    db  "  address: ", 0
+.str_references:
+    db  "  references: ", 0
 .ENDFRAME
 
 .EOF
