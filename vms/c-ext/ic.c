c5c3f5328fef24fbdbc7a37a401932b58d15f125
Matus Horvath <matus.horvath@gmail.com>
2024-08-03 15:44:00 +0200

    Separate extended IC VM

--- vms/c-ext/ic.c
+++ vms/c-ext/ic.c
@@ -3,11 +3,52 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "profile.h"
+#include "signals.h"
+#include "terminal.h"
+
 int *mem = NULL;
 int mem_size = 0;
 
 int ip = 0;
 int rb = 0;
+
+int extended = false;
+
+void parse_command_line(int argc, char **argv, char **program_name, bool *enable_profile, bool *enable_extended) {
+    *program_name = NULL;
+    *enable_profile = false;
+    *enable_extended = false;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0) {
+            *enable_profile = true;
+        } else if (strcmp(argv[i], "-e") == 0) {
+            *enable_extended = true;
+        } else {
+            *program_name = argv[i];
+        }
+    }
+}
+
+void init_extended_vm(void) {
+    extended = false;
+
+    // Check whether 'jnz 0, addr' is the first instruction
+    if (mem_size < 3 || mem[0] != 1105 || mem[1] != 0) {
+        return;
+    }
+
+    // Check if the extended starting address is valid
+    int address = mem[2];
+    if (address < 0 || address >= mem_size) {
+        return;
+    }
+
+    // Use the extended starting address instead of 0
+    extended = true;
+    ip = address;
+}
 
 void resize_mem(int addr) {
     if (addr >= mem_size) {
@@ -16,6 +57,8 @@ void resize_mem(int addr) {
         mem = (int *)realloc(mem, mem_size * sizeof(int));
         memset(mem + old_mem_size, 0, (mem_size - old_mem_size) * sizeof(int));
     }
+
+    resize_profile(addr);
 }
 
 int get_mem(int addr) {
@@ -60,7 +103,19 @@ void set_param(int idx, int val) {
     }
 }
 
+int is_feature(int id) {
+    switch (id) {
+        case 13: return 1;
+        default: return 0;
+    }
+}
+
 int get_input() {
+    return getc(stdin);
+}
+
+int get_input_async() {
+    // TODO implement async input
     return getc(stdin);
 }
 
@@ -126,25 +181,36 @@ void run() {
                 break;
             case 99: // hlt
                 return;
+            case 10: // ftr
+                if (extended) {
+                    // ftr ftrid, [dst]: set [dst] to 1 if feature ftrid is supported
+                    set_param(1, is_feature(get_param(0)) ? 1 : 0);
+                    ip += 3;
+                    break;
+                }
+            case 13: // ina
+                if (extended) {
+                    // ina [dst]: asynchronous input, read a character if available, otherwise set [dst] to -1
+                    set_param(0, get_input_async());
+                    ip += 2;
+                    break;
+                }
             default:
                 fprintf(stderr, "opcode error: ip %d oc %d\n", ip, oc);
                 exit(1);
         }
+
+        update_profile(ip);
+        handle_signals();
     }
 }
 
-int main(int argc, char **argv) {
-#ifdef _WIN32
-    _setmode(_fileno(stdin), _O_BINARY);
-    _setmode(_fileno(stdout), _O_BINARY);
-    _setmode(_fileno(stderr), _O_BINARY);
-#endif // _WIN32
-
+void load_binary(char *program_name) {
     mem_size = 64;
     mem = (int*)malloc(mem_size * sizeof(int));
     memset(mem, 0, mem_size * sizeof(int));
 
-    FILE *input = fopen(argv[1], "rt");
+    FILE *input = fopen(program_name, "rt");
 
     int idx = 0;
     char ch = ',';
@@ -157,8 +223,29 @@ int main(int argc, char **argv) {
         }
         set_mem(idx++, num);
     }
+}
+
+int main(int argc, char **argv) {
+    char *program_name;
+    bool enable_profile, enable_extended;
+    parse_command_line(argc, argv, &program_name, &enable_profile, &enable_extended);
+
+    load_binary(program_name);
+
+    if (enable_extended) {
+        init_extended_vm();
+    }
+
+    init_terminal(extended);
+    init_signals();
+
+    if (enable_profile) {
+        init_profile(program_name);
+    }
 
     run();
+
+    save_profile();
 
     return 0;
 }
