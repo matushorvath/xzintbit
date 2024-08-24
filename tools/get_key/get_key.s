42d44fb56c47536ef14c290941a28fb0e52f4f35
Matus Horvath <matus.horvath@gmail.com>
2024-08-24 12:16:35 +0200

    Print new line before each escape code in get_key

--- tools/get_key/get_key.s
+++ tools/get_key/get_key.s
@@ -16,6 +16,13 @@ extended_init:
 loop:
     in  [char]
 
+    eq  [char], 0x1b, [tmp]
+    jz  [tmp], after_eol
+
+    out 10
+    out 13
+
+after_eol:
     add [char], 0, [rb - 1]
     arb -1
     call print_num_16_b
@@ -25,6 +32,8 @@ loop:
     jz  0, loop
 
 char:
+    db  0
+tmp:
     db  0
 
     ds 1000, 0
