1dbb513a9afd81a78e305140f5894eea8b90035f
Matus Horvath <matus.horvath@gmail.com>
2024-09-18 15:12:17 +0200

    ASCII printing works, but not with cobol-85

--- vms/cobol/ic.cob
+++ vms/cobol/ic.cob
@@ -16,16 +16,16 @@
        FD output-file.
       *    RECORD IS 1 CHARACTERS.
        01  output-char
-           PICTURE IS X(1).
-      *    USAGE IS BINARY.
+      *    PICTURE IS 999
+           USAGE IS BINARY-CHAR UNSIGNED.
       *FD input-file
       *01  input-char.
 
        PROCEDURE DIVISION.
            OPEN OUTPUT output-file.
-           WRITE output-char FROM "X".
-           WRITE output-char FROM "Y".
-           WRITE output-char FROM "Z".
+           WRITE output-char FROM 65.
+      *    WRITE output-char FROM "Y".
+      *    WRITE output-char FROM "Z".
            CLOSE output-file.
 
            DISPLAY "Hello, world!".
